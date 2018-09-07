ab3bcf7353acb33d0c531e5a0fcb12b52affe123
Hanifa Arrumaisha <hanifa.aisha@gmail.com>
2018-09-07 15:21:26 +0700

    Initial commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,24 @@
+
+public class Dosen {
+	private int NIP;
+	private String nama;
+	
+	public Dosen(int nIP, String nama) {
+		NIP = nIP;
+		this.nama = nama;
+	}
+	public int getNIP() {
+		return NIP;
+	}
+	public void setNIP(int nIP) {
+		NIP = nIP;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	
+	
+}
